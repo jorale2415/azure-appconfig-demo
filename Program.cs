7546c8230ce33246275c20a4f9f93f792b99c358
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2024-09-19 22:05:56 -0500

    Creating repos for testing azure app configuration and azure key vault

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace AzureAppConfigDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddAzureAppConfiguration(options =>
+                {
+                    options.Connect(new Uri("https://app-config-secret-mgmt.azconfig.io"), new DefaultAzureCredential())
+                           .ConfigureKeyVault(kv =>
+                           {
+                               kv.SetCredential(new DefaultAzureCredential());
+                           });
+                });
+
+            var configuration = builder.Build();
+
+            // Example of retrieving a setting
+            var apiOptionsKey = configuration["ApiOptionsKey"];
+            Console.WriteLine($"ApiOptionKey: {apiOptionsKey}");
+
+            // Example of retrieving a secret from Key Vault
+            var apiOptionsSecret = configuration["ApiOptionsSecret"];
+            Console.WriteLine($"ApiOptionsSecret: {apiOptionsSecret}");
+        }
+    }
+}
